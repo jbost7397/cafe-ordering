0ac01bfe9831d077a7182d4f3a5ba4da451c90a5
mattdays <35571381+mattdays@users.noreply.github.com>
2018-03-20 15:24:42 -0400

    Create php version of index

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cafe Ordering</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!--<link href="style.css" rel="stylesheet">-->
+    </head>
+    <body>
+        <?php
+         echo "<h1>Order stuff online!</h1>";
+        ?>
+    </body>
+</html>
